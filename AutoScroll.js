eb068b465911e95464ae6fa4e809ef416c6f6606
Kishan B <kishancs46@gmail.com>
2016-09-07 12:04:36 +0530

    Create AutoScroll.js

---
+++ AutoScroll.js
@@ -0,0 +1,38 @@
+var Scroll = function(){
+
+    var intervalId;
+
+    this.start = function(options){
+        var isForwardScroll = options.forward;
+        var speed = options.speed;
+        var interval = 1, scrollLength = 1;
+
+        switch(speed){
+          case 'slow'       : interval = 80;
+                              break;
+          case 'medium'     : interval = 30;
+                              break;
+          case 'fast'       : interval = 1;
+                              break;
+          case 'superfast'  : interval = 1;
+                              scrollLength = 10;
+                              break;
+          case 'extreme'    : interval = 1;
+                              scrollLength = 100;
+                              break;
+        }
+
+        intervalId = setInterval(function(){
+          if(isForwardScroll){
+             document.body.scrollTop += scrollLength;
+          }else{
+            document.body.scrollTop -= scrollLength;
+          }
+       }, interval);
+    }
+
+    this.stop = function(){
+      clearInterval(intervalId);
+    }
+
+};
