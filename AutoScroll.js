7e99ff49b1ea949447d3d77456fce869d5c964b8
Kishan B <kishancs46@gmail.com>
2016-11-08 15:39:24 +0530

    Update AutoScroll.js

--- AutoScroll.js
+++ AutoScroll.js
@@ -1,38 +1,40 @@
-var AutoScroll = function(){
+var AutoScroll = (function(){
 
     var intervalId;
 
-    this.start = function(options){
-        var isForwardScroll = options.forward;
-        var speed = options.speed;
-        var interval = 1, scrollLength = 1;
+    return {
+        start: function(options) {
+            var isForwardScroll = options.forward;
+            var speed = options.speed;
+            var interval = 1, scrollLength = 1;
 
-        switch(speed){
-          case 'slow'       : interval = 80;
-                              break;
-          case 'medium'     : interval = 30;
-                              break;
-          case 'fast'       : interval = 1;
-                              break;
-          case 'superfast'  : interval = 1;
-                              scrollLength = 10;
-                              break;
-          case 'extreme'    : interval = 1;
-                              scrollLength = 100;
-                              break;
+            switch(speed){
+              case 'slow'       : interval = 80;
+                                  break;
+              case 'medium'     : interval = 30;
+                                  break;
+              case 'fast'       : interval = 1;
+                                  break;
+              case 'superfast'  : interval = 1;
+                                  scrollLength = 10;
+                                  break;
+              case 'extreme'    : interval = 1;
+                                  scrollLength = 100;
+                                  break;
+            }
+
+            intervalId = setInterval(function(){
+              if(isForwardScroll){
+                 document.body.scrollTop += scrollLength;
+              }else{
+                document.body.scrollTop -= scrollLength;
+              }
+           }, interval);
+        },
+        
+        stop: function(){
+          clearInterval(intervalId);
         }
+    };
 
-        intervalId = setInterval(function(){
-          if(isForwardScroll){
-             document.body.scrollTop += scrollLength;
-          }else{
-            document.body.scrollTop -= scrollLength;
-          }
-       }, interval);
-    }
-
-    this.stop = function(){
-      clearInterval(intervalId);
-    }
-
-};
+})();
